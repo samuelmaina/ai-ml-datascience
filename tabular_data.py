a764c57ad4f12f0313fa63995aec2b159809d9f4
samuelmayna <samuelmayna@gmail.com>
2021-12-23 21:37:10 +0300

    Add methods to read the data from the first and the last lines

--- tabular_data.py
+++ tabular_data.py
@@ -1,5 +1,15 @@
+import matplotlib
+
 import numpy as np
 import pandas as pd
 
+#pd loads the dat into dataframe
 df=pd.read_csv('PastHires.csv')
-df.head()
+
+#read the first few values from a dataflame
+print(df.head()) #the default being 5
+print(df.head(10)) # one can specify the vales that they need to view.
+
+print(df.tail()) #prints the last five records 
+print(df.tail(10)) #prints the last specific records
+
