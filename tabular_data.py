c576456e7f86c6bb3fee8569edc20f647fc514de
samuelmayna <samuelmayna@gmail.com>
2021-12-23 22:02:56 +0300

    Add methods of extracting data from a df

--- tabular_data.py
+++ tabular_data.py
@@ -13,3 +13,25 @@ print(df.head(10)) # one can specify the vales that they need to view.
 print(df.tail()) #prints the last five records 
 print(df.tail(10)) #prints the last specific records
 
+
+print(df.shape) #used to get the number rows and columns
+print(df.size) #used to get the total number of cells in the data
+ 
+ 
+print(df.columns) #used to get the name of columns in the data frame.
+
+print(df['Hired']) 
+                    #used to extract data from the given column and load it 
+                    #load it to a new df. Mostly only some few columns are need for
+                    #data manipulation. They a
+                   
+
+print(df["Hired"][:5]) #used to extract the first five rows from
+                  #the selected columns
+
+print(df["Hired"][5]) #used to extract an exact value
+
+#to print the number of unique data values in a df
+print( df['Level of Education'].value_counts())
+
+
