1703755054c053c0e6f06900bdac637a889bc566
samuelmayna <samuelmayna@gmail.com>
2021-12-28 12:44:04 +0300

    Add method of displaying using the matplotlib's histogram method

--- centers_of_tendencies.py
+++ centers_of_tendencies.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 
 #use the np module to generate 10,000 random data with normal distribution 
 #with the mean of 27,000, with standard deviation of 15,000
-income = np.random.normal(27000, 15000, 10000)
+incomes = np.random.normal(27000, 15000, 10000)
 
-print(np.mean(income))
+print(np.mean(incomes))
+
+#generate a histogram with 50 strips
+plt.hist(incomes,50)
+plt.show()
