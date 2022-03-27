b103881ac15c3ce5bfd753b1548940cfc3169e38
samuelmayna <samuelmayna@gmail.com>
2022-03-27 23:15:24 +0300

    Add normal distribution generation

--- distributions.py
+++ distributions.py
@@ -1,6 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.stats import norm
+
+
+#uniform distribution
 values= np.random.uniform(-10.0,10.0, 100000)
 plt.hist(values, 50)
+plt.show()
+
+#normal distribution
+x= np.arange(-3,3, 0.0001)
+plt.plot(x,norm.pdf(x));
 plt.show()
