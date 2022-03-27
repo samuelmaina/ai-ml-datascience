65c97db43356b85cbefbabe0ba1ebaa51405a40f
samuelmayna <samuelmayna@gmail.com>
2022-03-27 22:15:25 +0300

    Add the a histogram to the data

--- centers_of_tendencies.py
+++ centers_of_tendencies.py
@@ -7,6 +7,27 @@ incomes = np.random.normal(27000, 15000, 10000)
 
 print(np.mean(incomes))
 
+
+
+print(np.median(incomes))
+
+#Median is less suspectible to outliers than mean.
+
+
+#the append method is used to concatenate two arrays 
+original_incomes= incomes
+incomes= np.append(incomes,[10000000000])
+
+#the first run of the program the mean was 1026988.1125744453 and the median is 26929.598072966848 in which 26929 is more close
+#to the center than the mean.
+print("this is the mean affter outlier" ,np.mean(incomes))
+print("This is the median after adding an outlier",np.median(incomes))
+
+
+#generate random 500 integers from 18 to an upperbound of 90
+ages= np.random.randint(18, high=90, size=500)
+print(ages)
+
 #generate a histogram with 50 strips
-plt.hist(incomes,50)
+plt.hist(original_incomes,50)
 plt.show()
