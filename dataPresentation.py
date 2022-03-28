a3723edb60a23a0cc04dd442e3541ae536c9c857
samuelmayna <samuelmayna@gmail.com>
2022-03-28 18:46:57 +0300

    Add  the displaying of two graphs on the same axex and the method to save the graphs into a certain file

--- dataPresentation.py
+++ dataPresentation.py
@@ -3,7 +3,28 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 
 
-#showing a single graph on the same axes
+#showing a single graph on some axis
+plt.xlabel("Incomes")
+plt.ylabel("No of values")
 x= np. arange(-3, 3, 0.001)
 plt.plot(x,norm.pdf(x))
 plt.show()
+
+#multiple plots on the same graph
+plt.xlabel("Incomes")
+plt.ylabel("No of values")
+
+plt.plot(x,norm.pdf(x))
+plt.plot(x, norm.pdf(x, 1.0, 0.5))
+plt.show()
+
+#saving the content into a file
+plt.xlabel("Incomes")
+plt.ylabel("No of values")
+plt.plot(x, norm.pdf(x))
+plt.plot(x, norm.pdf(x, 1.0, 0.5))
+plt.savefig("myPlot.png", format= "png")
+
+
+
+
