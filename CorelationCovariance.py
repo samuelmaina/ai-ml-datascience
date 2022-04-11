444788785184527d74103d7c0523961c459d4729
samuelmayna <samuelmayna@gmail.com>
2022-04-11 22:04:45 +0300

    implemented co-relation

--- CorelationCovariance.py
+++ CorelationCovariance.py
@@ -2,6 +2,8 @@ from numpy import mean,dot
 import matplotlib.pyplot as plt
 
 import numpy as np
+
+
 def de_mean(x):
     xmean= np.mean(x)
     return [x1 - xmean for x1 in x]
@@ -15,6 +17,19 @@ pageSpeeds=np.random.normal(3.0, 1.0, 1000)
 purchaseAmount= np.random.normal(50.0, 10.0, 1000)
 
 plt.scatter(pageSpeeds,purchaseAmount)
+plt.show()
 print(coveriance(pageSpeeds, purchaseAmount))
+
+
+
+def correlation(x, y):
+    stddevx= x.std()
+    stddevy= y.std()
+    return coveriance(x, y)/stddevx/stddevy
+
+
+print(correlation(pageSpeeds, purchaseAmount))
+
+
 
 
