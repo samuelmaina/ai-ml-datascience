a36344427fc16b7c126d53b67f4a723546c5a6a7
samuelmayna <samuelmayna@gmail.com>
2022-05-12 15:17:51 +0300

    Tested the model with sample text.

--- naiveBayesSpamClassifier.py
+++ naiveBayesSpamClassifier.py
@@ -1,8 +1,8 @@
-from fileinput import filename
+
 import os
 import io
-from posixpath import dirname
-import numpy as np
+
+
 from pandas import DataFrame
 from sklearn. feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -36,5 +36,28 @@ data= data.append(dataFrameFromDirectory('emails/spam','spam'))
 data= data.append(dataFrameFromDirectory('emails/ham','ham'))
 print(data.head())
 
+vectorizer= CountVectorizer()
+#token each work into a numbers
+counts= vectorizer.fit_transform(data['message'].values)
+
+
+#perform the  the Naive Bayers.
+classifier= MultinomialNB() 
+target= data['class'].values
+classifier.fit(counts, target)
+
+
+test= ['Low selling viagra now!!', "Hi Samuel, how about a hackthon tomorrow?"]
+
+test_counts= vectorizer.transform(test)
+
+
+
+#test the model to see if it can if identify the first message as spam and the second one
+#as ham.
+predictions= classifier.predict(test_counts)
+print(predictions)
+
             
+    
     
